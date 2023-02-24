cbd6eb6722f0666085b3e4b9547f8b5e8384d971
alvin-reyes <me@areyes.onl>
2023-02-24 14:57:40 -0500

    tracer

--- main.go
+++ main.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/application-research/delta-db/db_models"
 	"github.com/application-research/delta-db/messaging"
-	"github.com/nsqio/go-nsq"
-	"log"
-	"time"
 )
 
 func main() {
-	// Instantiate a new producer that publishes to the specified topic
-	producer, err := nsq.NewProducer(messaging.MetricsTopicUrl, nsq.NewConfig())
-	if err != nil {
-		log.Fatal(err)
-	}
+	producer := messaging.NewDeltaMetricsMessageProducer()
+	// add a handler to consume messages
+	producer.Publish([]byte("Hello World!"))
 
-	// Publish a message to the topic
-	data := db_models.Content{
-		ID:                0,
-		Name:              "",
-		Size:              0,
-		Cid:               "",
-		RequestingApiKey:  "",
-		PieceCommitmentId: 0,
-		Status:            "",
-		ConnectionMode:    "",
-		LastMessage:       "",
-		CreatedAt:         time.Time{},
-		UpdatedAt:         time.Time{},
-	}
-
-	message, err := json.Marshal(data)
-	err = producer.Publish(messaging.PrimaryTopic, message)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Gracefully stop the producer when done
-	producer.Stop()
 }
--- messaging/base.go
+++ messaging/base.go
@@ -1,11 +1,43 @@
 package messaging
 
+import "encoding/json"
+
 var (
 	MetricsTopicUrl = "145.40.77.207:4150"
 	PrimaryTopic    = "delta-metric-events"
+	producer        *DeltaMetricsMessageProducer
 )
 
 type DeltaMetricsBaseMessage struct {
 	ObjectType string      `json:"object_type"`
 	Object     interface{} `json:"object"`
+}
+
+func init() {
+	producer = NewDeltaMetricsMessageProducer()
+}
+
+func (p *DeltaMetricsBaseMessage) Trace(message DeltaMetricsBaseMessage) error {
+	messageBytes, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	producer.Publish(messageBytes)
+	return nil
+}
+
+func (p *DeltaMetricsBaseMessage) TraceMessageString(message string) error {
+	err := producer.Publish([]byte(message))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *DeltaMetricsBaseMessage) TraceMessage(message []byte) error {
+	err := producer.Publish(message)
+	if err != nil {
+		return err
+	}
+	return nil
 }
