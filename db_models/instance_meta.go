d8bf98d5e4ef12be1002808465fe2f60f2a916e9
alvin-reyes <me@areyes.onl>
2023-03-04 23:09:41 -0500

    added more meta instance info

--- db_models/instance_meta.go
+++ db_models/instance_meta.go
@@ -17,6 +17,7 @@ type InstanceMeta struct {
 	DisableOnlineDeals               bool      `json:"disable_online_deals"`
 	DisableOfflineDeals              bool      `json:"disable_offline_deals"`
 	NumberOfCpus                     uint64    `json:"number_of_cpus"`
+	StorageInBytes                   uint64    `json:"storage_in_bytes"`
 	SystemMemory                     uint64    `json:"system_memory"`
 	HeapMemory                       uint64    `json:"heap_memory"`
 	HeapInUse                        uint64    `json:"heap_in_use"`
