1fda604342c27d055e51260353ba9253fe5aae09
alvin-reyes <me@areyes.onl>
2023-03-04 20:23:40 -0500

    added meta instance information to limit resources

--- db_models/instance_meta.go
+++ db_models/instance_meta.go
@@ -16,7 +16,13 @@ type InstanceMeta struct {
 	DisableStorageDeal               bool      `json:"disable_storage_deal"`
 	DisableOnlineDeals               bool      `json:"disable_online_deals"`
 	DisableOfflineDeals              bool      `json:"disable_offline_deals"`
+	NumberOfCpus                     uint64    `json:"number_of_cpus"`
+	SystemMemory                     uint64    `json:"system_memory"`
+	HeapMemory                       uint64    `json:"heap_memory"`
+	HeapInUse                        uint64    `json:"heap_in_use"`
+	StackInUse                       uint64    `json:"stack_in_use"`
 	InstanceStart                    time.Time `json:"instance_start"`
+	BytesPerCpu                      uint64    `json:"bytes_per_cpu"`
 	CreatedAt                        time.Time `json:"created_at"`
 	UpdatedAt                        time.Time `json:"updated_at"`
 }
